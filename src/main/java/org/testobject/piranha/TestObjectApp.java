17c4be08827bff87e86d5bdcd02368c853ff33a9
aluedeke <aluedeke@testobject.com>
2015-06-30 10:42:15 +0200

    upload apk with md5 from file to make sure that files are only uploaded once

---
+++ src/main/java/org/testobject/piranha/TestObjectApp.java
@@ -0,0 +1,12 @@
+package org.testobject.piranha;
+
+/**
+ * Created by aluedeke on 25.06.15.
+ */
+public class TestObjectApp {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+}
